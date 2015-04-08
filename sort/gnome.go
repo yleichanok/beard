1c7624ec09eb7c8a5102decf6d6b45a3f092cceb
Evgeny Leychenok <e.leychenok@gmail.com>
2015-04-07 19:09:42 -0700

    Added gnome sort

---
+++ sort/gnome.go
@@ -0,0 +1,36 @@
+package sort
+
+// Gnome sort algorithm.
+//
+// Puts items in order by comparing the current item with the previous item.
+// If they are in order, move to the next item (or stop if the end is reached).
+// If they are out of order, swap them and move to the previous item.
+// If there is no previous item, move to the next item.
+//
+// See http://en.wikipedia.org/wiki/Gnome_sort.
+func GnomeSort(arr Sortable) {
+
+	if arr == nil {
+		panic("arr is nil")
+	}
+
+	arrLen := arr.Len()
+
+	// Quit if nothing to sort.
+	if arrLen < 2 {
+		return
+	}
+
+	for i := 1; i < arrLen; {
+		if !arr.Less(i, i-1) {
+			i++
+		} else {
+			arr.Swap(i, i-1)
+			if i > 1 {
+				i--
+			}
+		}
+	}
+
+	return
+}
